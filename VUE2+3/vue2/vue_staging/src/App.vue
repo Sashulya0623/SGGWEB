28ba678f654b6e9ef725722d18a6cd624b6f5197
vincy-hhr-own <yan1911881209@163.com>
2022-03-27 21:08:02 +0800

    echarts单位

--- VUE2+3/vue2/vue_staging/src/App.vue
+++ VUE2+3/vue2/vue_staging/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-   <h1>{{count}}</h1>
-   <button @click="count++">+</button>
-   <button @click="count--">-</button>
-   <Test v-show="count === 1"/>
-
-   <Test2 />
-   <Test3 />
-   <Test4 />
-   <Test5 />
-   <Test6 />
-   <Test7 />
-   <Test8 />
+    <div v-if="show">
+      <h1>{{count}}</h1>
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+      <Test v-show="count === 1"/>
+      <Test2 />
+      <Test3 />
+      <Test4 />
+      <Test5 />
+      <Test6 />
+      <Test7 />
+      <Test8 />
+    </div>
+   
+    <div v-if="!show">
+      <Test9 />
+    </div>
     
   </div>
 </template>
@@ -25,11 +30,13 @@ import Test5 from './components/Test5'
 import Test6 from './components/Test6'
 import Test7 from './components/Test7'
 import Test8 from './components/Test8'
+import Test9 from './components/Test9'
 export default {
   name: 'App',
   data() {
     return {
-      count: 0
+      count: 0,
+      show: false
     }
   },
   components: {
@@ -41,6 +48,7 @@ export default {
     Test6,
     Test7,
     Test8,
+    Test9,
   }
 }
 </script>
---
+++ VUE2+3/vue2/vue_staging/src/components/Test9.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="test">
+    <div ref="test" class="main"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "Test",
+  data() {
+      return {
+          option: {}
+      }
+  },
+  methods: {
+    init() {
+      const box1 = echarts.init(this.$refs.test)
+
+      
+      this.option = {
+        title: {
+          text: "企业能耗占比",
+          subtext: "下月预计能耗",
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{b}: {c}度 (占比{d}%)",
+        },
+        legend: {
+        //   orient: "vertical",
+        //   left: "left",
+        },
+        series: [
+          {
+            name: "下月预计能耗(度)",
+            type: "pie",
+            radius: "50%",
+            data: [
+              { value: 1048, name: "Search Engine" },
+              { value: 735, name: "Direct" },
+              { value: 580, name: "Email" },
+              { value: 484, name: "Union Ads" },
+              { value: 300, name: "Video Ads" },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      }
+
+      box1.setOption(this.option)
+    },
+  },
+  mounted() {
+    this.init();
+  },
+};
+</script>
+
+<style scoped>
+.test {
+  width: 100%;
+}
+
+.main {
+  height: 400px;
+}
+</style>
